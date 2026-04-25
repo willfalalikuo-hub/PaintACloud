a204cde9f11d417e6f9d5ab93961c0445084b422
willfalalikuo-hub <willfalalikuo-hub@users.noreply.github.com>
2026-04-25 15:52:17 +0800

    deploy: 黑白灰简约风格

---
+++ images/references/.nuxt/dist/server/client.manifest.mjs
@@ -0,0 +1,4 @@
+
+import { viteNodeFetch } from "file:///Users/mac/Workspace/xschool/paintCloud/node_modules/@nuxt/vite-builder/dist/vite-node.mjs"
+export default () => viteNodeFetch.getManifest()
+    
